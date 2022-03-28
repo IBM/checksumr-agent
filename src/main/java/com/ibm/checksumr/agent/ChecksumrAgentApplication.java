93ebdefdb84d4b60d0fa4a4679382a445495e2d4
Denys Xavier <denysxavier.github@outlook.com>
2022-03-28 13:29:19 -0300

    Added Springboot project template from Initializr

---
+++ src/main/java/com/ibm/checksumr/agent/ChecksumrAgentApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.checksumr.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChecksumrAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChecksumrAgentApplication.class, args);
+	}
+
+}
