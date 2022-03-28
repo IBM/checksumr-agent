f2800bae327ceaf2afadc3fdff9791fdae941985
Denys Xavier <denysxavier.github@outlook.com>
2022-03-28 13:56:46 -0300

    Setup Springboot app to run as command line

--- src/main/java/com/ibm/checksumr/agent/ChecksumrAgentApplication.java
+++ src/main/java/com/ibm/checksumr/agent/ChecksumrAgentApplication.java
@@ -1,13 +1,20 @@
 package com.ibm.checksumr.agent;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChecksumrAgentApplication {
+public class ChecksumrAgentApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChecksumrAgentApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+
 	}
 
 }
