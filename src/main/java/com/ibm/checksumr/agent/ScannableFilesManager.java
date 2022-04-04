88de139fa7abe38df1c48ba3eee5e4338683c158
Denys Xavier <denysxavier.github@outlook.com>
2022-04-04 00:42:58 -0300

    Added function to scan and list all files from paths

--- src/main/java/com/ibm/checksumr/agent/ScannableFilesManager.java
+++ src/main/java/com/ibm/checksumr/agent/ScannableFilesManager.java
@@ -1,10 +1,15 @@
 package com.ibm.checksumr.agent;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ScannableFilesManager {
-	
 	public static Path[] toPaths(String[] stringsOfPath) {
 		Path[] paths = new Path[stringsOfPath.length];
 
@@ -13,6 +18,21 @@ public class ScannableFilesManager {
 		}
 
 		return paths;
+	}
+
+	public static List<Path> listFilepaths(Path... pathsToSearch) {
+		List<Path> directoryListing = new ArrayList<Path>();
+
+		for (Path pathToSearch : pathsToSearch) {
+			try (Stream<Path> stream = Files.walk(pathToSearch)) {
+				directoryListing.addAll(stream.filter(Files::isRegularFile).collect(Collectors.toList()));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return directoryListing;
 	}
 
 }
--- src/test/java/com/ibm/checksumr/agent/ScannableFilesManagerTests.java
+++ src/test/java/com/ibm/checksumr/agent/ScannableFilesManagerTests.java
@@ -4,13 +4,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.github.javafaker.Faker;
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
 
 @SpringBootTest
 public class ScannableFilesManagerTests {
@@ -35,6 +42,37 @@ public class ScannableFilesManagerTests {
 		assertEquals(numberOfItems, arrayOfPath.length);
 		for (int i = 0; i < stringsOfPath.length; i++) {
 			assertTrue(arrayOfPath[i].endsWith(stringsOfPath[i]));
+		}
+	}
+
+	@Test
+	void ensureAllAbsoluteFilePathsFromAGivenPathListAreListed() throws IOException {
+		Path rootReference, fakeFileReference;
+		List<Path> listOfPaths = new ArrayList<Path>();
+		FileSystem virtualFileSystem = Jimfs.newFileSystem(Configuration.unix());
+
+		Path app1BaseDir = virtualFileSystem.getPath(faker.app().name());
+		Path app2BaseDir = virtualFileSystem.getPath(faker.app().name());
+		Files.createDirectory(app1BaseDir);
+		Files.createDirectory(app2BaseDir);
+
+		for (int i = 0; i < 10; i++) {
+			rootReference = app1BaseDir;
+			if (i % 2 == 0) {
+				rootReference = app2BaseDir;
+			}
+
+			fakeFileReference = rootReference.resolve(faker.file().fileName("", null, null, ""));
+			listOfPaths.add(fakeFileReference);
+			Files.createFile(fakeFileReference);
+		}
+		Collections.sort(listOfPaths);
+
+		List<Path> directoryListing = ScannableFilesManager.listFilepaths(app1BaseDir, app2BaseDir);
+		Collections.sort(directoryListing);
+
+		for (int i = 0; i < listOfPaths.size(); i++) {
+			assertEquals(0, directoryListing.get(i).compareTo(listOfPaths.get(i)));
 		}
 	}
 }
